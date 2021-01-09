c4d6420cb530e486e910b84c544b784a61a2b21e
codmic1330 <ch20026103@gmail.com>
2021-01-09 15:57:49 +0800

    react

---
+++ src/check.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('1+1=2', () => {
+  
+  let a = 1;
+  let b = 1;
+  let c = a+b;
+  expect(c).toBe(3);
+});
